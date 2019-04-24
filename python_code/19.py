a4e68750225f9375cf1bad2b183cb49d8732cfc5
[Wolf] <[171982919@qq.com]>
2019-04-24 23:09:17 +0800

    change 19.py

--- python_code/19.py
+++ python_code/19.py
@@ -2,9 +2,9 @@ import os
 
 def get_pycfile(dir, suffix):
     res = []
-    for root, dirs, files in os.walk(dir):
+    for root, _, files in os.walk(dir):
         for filename in files:
-            name, suf = os.path.splitext(filename)
+            _, suf = os.path.splitext(filename)
             if suf == suffix:
                 res.append(os.path.join(root, filename))
 
