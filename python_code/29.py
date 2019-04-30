e44eaea06de871284f404bee17aa790927725873
[Wolf] <[171982919@qq.com]>
2019-04-30 15:12:34 +0800

    modified 29.py and another solution

--- python_code/29.py
+++ python_code/29.py
@@ -1,3 +1,4 @@
+# 1 
 class Solution:
     def two_sum(self, nums, target):
         '''
@@ -15,9 +16,21 @@ class Solution:
                 d[nums[size]] = size
             size = size + 1
 
+# 2
+from typing import List 
+# 用于类型检查，防止运行时出现参数与返回值不符合
+def two_sum(nums: List[int], target:int) -> List[int]:
+    s = {}
+    for i, n in enumerate(nums):
+        if (target - n) in s :
+            return [s[target - n], i]
+        else:
+            s[n] = i
+
 if __name__ == "__main__":
-    solu = Solution()
+    #solu = Solution()
     num = [2, 7, 2, 11, 15]
     target = 9
-    nums = solu.two_sum(num, target)
+    nums = two_sum(num, target)
+    #nums = solu.two_sum(num, target)
     print(nums)
