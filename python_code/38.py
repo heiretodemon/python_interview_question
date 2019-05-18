6536168eb5bbd2fdc950c7e822eaa19b10235f16
[Wolf] <[171982919@qq.com]>
2019-05-18 21:20:02 +0800

    update 38.py

--- python_code/38.py
+++ python_code/38.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def find_2nd_num_1(num_list):
     '''
     方法1：直接进行逆序排序，找出第二个数即可
@@ -19,7 +21,12 @@ def find_2nd_num_2(num_list):
             b = num_list[i]
     return b
 
+def find_2nd_num_3(num_list):
+    num = reduce(lambda ot, x: ot[1]<x and (ot[1], x) or ot[0]< x 
+    and (x, ot[1]) or ot, num_list, (0,0))[0]
+    return num
+
 
 if __name__ == '__main__':
     num_list = [34, 11, 23, 56, 78, 0, 9, 12, 3, 7, 5]
-    print(find_2nd_num_2(num_list))
+    print(find_2nd_num_3(num_list))
