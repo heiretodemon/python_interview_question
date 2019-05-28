fbb360d9c0f8deb33683afd7ea4d6343a73c743d
[Wolf] <[171982919@qq.com]>
2019-05-28 22:43:31 +0800

    update to 44.py

---
+++ python_code/44.py
@@ -0,0 +1,28 @@
+class Test:
+    __list = []
+
+    def __init__(self):
+        print("construction")
+
+    def __del__(self):
+        print("destruct")
+
+    def __str__(self):
+        return "a string version of Test"
+
+    def __getitem__(self, key):
+        return self.__list[key]
+
+    def len(sefl):
+        return len(self.__list)
+
+    def Add(self, value):
+        self.__list.append(value)
+
+    def Remove(self, index):
+        del self.__list[index]
+
+    def DisplayItems(self):
+        for i in self.__list:
+            print(i)
+    
