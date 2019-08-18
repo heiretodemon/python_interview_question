4f5b1137d44df644cad41e207ff4b697a17d15a3
heiretodemon <171982919@qq.com>
2019-08-18 16:16:30 +0800

    modified the 83.py

--- python_code/83.py
+++ python_code/83.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
 '''
 
+# 无参数装饰器
 def add_symbol(func1):
     def prt_func(name):
         return '&&&{0}&&&'.format(func1(name))
@@ -25,6 +26,19 @@ def print_name(name):
 
 # 这样与上面的函数嵌套函数效果一致，就是将函数和字符串都
 # 都作为参数传递给装饰器了
+
+# 带参数的装饰器
+
+def add_symbol(symbol):
+    def decorator(func1):
+        def prt_func(name):
+            return '{0}&&&{1}&&&{0}'.format(symbol, func1(name))
+        return prt_func
+    return decorator
+
+@add_symbol('$$')
+def print_name(name):
+    return 'fuck, ' + name
 
 if __name__ == "__main__":
     print (print_name('world'))
