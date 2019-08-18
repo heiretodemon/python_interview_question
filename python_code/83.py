4e1c473723ee27a6fcb1199dbb9fec2eac5217c1
heiretodemon <171982919@qq.com>
2019-08-18 16:04:05 +0800

    modified the 83.p

--- python_code/83.py
+++ python_code/83.py
@@ -1,3 +1,19 @@
+'''
+函数嵌套函数
+def print_name(name):
+    return 'fuck, ' + name
+
+def add_symbol(func1):
+    def prt_func(name):
+        return '&&&{0}&&&'.format(func1(name))
+    return prt_func
+
+if __name__ == "__main__":
+    hell = add_symbol(print_name)
+    print(hell('world'))
+
+'''
+
 def add_symbol(func1):
     def prt_func(name):
         return '&&&{0}&&&'.format(func1(name))
@@ -7,6 +23,8 @@ def add_symbol(func1):
 def print_name(name):
     return 'fuck, ' + name
 
-if __name__ == "__main__":
+# 这样与上面的函数嵌套函数效果一致，就是将函数和字符串都
+# 都作为参数传递给装饰器了
 
-    print(print_name('world'))
+if __name__ == "__main__":
+    print (print_name('world'))
