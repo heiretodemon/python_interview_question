5330f648325b6452238d714c8fb55dc084cc430c
heiretodemon <171982919@qq.com>
2020-02-19 14:42:43 +0800

    update 94.py

---
+++ python_code/94.py
@@ -0,0 +1,12 @@
+import re
+
+addr = '192.168.0.1'
+
+ip = re.search(r'(([01]{0,1}\d{0,1}\d| \
+    2[0-4]\d|25[0-5])\.){3}([01]{0,1}\d{0,1}\d|2[0-4]\d|25[0-5])',addr)
+print(ip)
+
+# 95
+a = "abbbccc"
+a = re.sub(r'b+','b', a)
+print(a)
